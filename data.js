2377d571b8ae3befc4edccebb0144bae859c1db1
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-13 11:57:40 -0300

    Décimo

--- data.js
+++ data.js
@@ -1,27 +1,18 @@
 let movies = [
     {
         name: 'falcon and the winter soldier',
-        des: 'Lorem ipsum dolor',
+        des: 'Lorem ipsum dolor sit amet',
         image: 'images/slider 2.PNG'
     },
     {
         name: 'loki',
-        des: 'Lorem ipsum dolor',
+        des: 'Lorem ipsum dolor sit amet',
         image: 'images/slider 1.PNG'
     },
     {
         name: 'wanda vision',
-        des: 'Lorem ipsum dolor',
+        des: 'Lorem ipsum dolor sit amet',
         image: 'images/slider 3.PNG'
     },
-    {
-        name: 'raya and the last dragon',
-        des: 'Lorem ipsum dolor',
-        image: 'images/slider 4.PNG'
-    },
-    {
-        name: 'luca',
-        des: 'Lorem ipsum dolor',
-        image: 'images/slider 5.PNG'
-    }
+/// and so on
 ]
