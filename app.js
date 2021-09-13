b98e73c7da46c8516f26a3c3d4ff04122b0306b0
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-13 13:44:26 -0300

    terceira parte

---
+++ app.js
@@ -0,0 +1,45 @@
+const carousel = document.querySelector('.carousel');
+let sliders = [];
+let slideIndex = 0;
+
+const createSlide = () =>{
+    if (slideIndex >= movies.length){
+        slideIndex = 0;
+    
+    }
+
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+
+    slide.className = 'slider';
+    content.className = 'slide-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-des';
+
+    sliders.push(slide);
+
+    if (sliders.lenght){
+        sliders[0].style.marginLeft = 'calc(-${100 * (slider.lenght -2)}% - ${30* (sliders.lenght -2)}px)';
+
+    }
+
+    for(let i =0; i<3; i++){
+        createSlide();
+    }
+    setInterval(() => {
+        createSlide();}, 3000);
+        
+}
