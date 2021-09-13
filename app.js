26b59e4a572a0aa63f2ac0fe8f7be08122e41517
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-13 16:31:57 -0300

    título

--- app.js
+++ app.js
@@ -43,3 +43,16 @@ const createSlide = () =>{
         createSlide();}, 3000);
         
 }
+
+const videoCards = [...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item =>{
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
