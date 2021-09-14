e15041476a9cef1bc8af629912753efb1d07c36a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-14 16:32:51 -0300

    Parte Final

--- app.js
+++ app.js
@@ -56,3 +56,20 @@ videoCards.forEach(item =>{
         video.pause();
     })
 })
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('nxt-btn')];
+
+cardContainers.forEach((item, i)=>{
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () =>{
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
