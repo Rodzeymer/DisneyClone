811e4042cb91e4a7c89629ebd1fe967e020d7626
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-15 09:10:28 -0300

    Testando JS 18

--- app.js
+++ app.js
@@ -7,22 +7,29 @@ const createSlide = () =>{
         slideIndex = 0;
     
     }
-
+ //creating DOM element
     let slide = document.createElement('div');
     let imgElement = document.createElement('img');
     let content = document.createElement('div');
     let h1 = document.createElement('h1');
     let p = document.createElement('p');
 
+// attaching all elements
+
     imgElement.appendChild(document.createTextNode(''));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
     p.appendChild(document.createTextNode(movies[slideIndex].des));
     content.appendChild(h1);
-    content.appendChild(imgElement);
+    content.appendChild(p);
+    slide.appendChild(imgElement);
     carousel.appendChild(slide);
+
+//setting up image
 
     imgElement.src = movies[slideIndex].image;
     slideIndex++;
+
+// setting elements classname
 
     slide.className = 'slider';
     content.className = 'slide-content';
@@ -31,9 +38,10 @@ const createSlide = () =>{
 
     sliders.push(slide);
 
+// adding sliding effect
+
     if (sliders.lenght){
         sliders[0].style.marginLeft = 'calc(-${100 * (slider.lenght -2)}% - ${30* (sliders.lenght -2)}px)';
-
     }
 
     for(let i =0; i<3; i++){
