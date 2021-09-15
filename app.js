05a640c0e164b4fe703b272cabe7c0b219f994bc
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-09-15 12:00:38 -0300

    483

--- app.js
+++ app.js
@@ -69,9 +69,11 @@ videoCards.forEach(item =>{
     })
 })
 
+//card sliders
+
 let cardContainers = [...document.querySelectorAll('.card-container')];
 let preBtns = [...document.querySelectorAll('.pre-btn')];
-let nxtBtns = [...document.querySelectorAll('nxt-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
 
 cardContainers.forEach((item, i)=>{
     let containerDimensions = item.getBoundingClientRect();
